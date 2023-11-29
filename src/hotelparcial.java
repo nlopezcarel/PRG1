ceae2f7909e8badcbf9bef203ba6f573d0e8cc91
nlopezcarel <nicolas.lopez@alumnos.uneatlantico.com>
2023-11-29 12:45:27 +0100

    Commits

--- src/hotelparcial.java
+++ src/hotelparcial.java
@@ -1,6 +1,26 @@
+import  java.util.Random;
+
 public class hotelparcial {
     
+    Random random = new Random();
+    
     public static void main(String[] args) {
+        final int dias = 7;
+        final int horas = 24;
+        final int pisos = 7;
+        final int habitacionesxpiso = 6;
+        final double probabilidadPA = 0.7;
+        final double probabilidadLE = 0.6;
+
+
+       
+            
+                     
+                
+                    
+
+
         
     }
+
 }
